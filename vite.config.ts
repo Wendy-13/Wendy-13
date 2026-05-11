586b40ac09cf151c0b048ba3f0db79a1f7732aee
Wendy-13 <a111182113@nkust.edu.tw>
2026-05-11 17:04:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     base:/'/wendy-13/'，
-    base: '/Wendy-13/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
